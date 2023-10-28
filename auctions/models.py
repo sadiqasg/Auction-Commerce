391d9844ba7c73dd138e1c48b289f00f4a5ce8ca
sadiqasg <sadiqasg@gmail.com>
2023-10-28 01:07:46 +0100

    cs50 project 2: updates

--- auctions/models.py
+++ auctions/models.py
@@ -5,13 +5,11 @@ from django.db import models
 class User(AbstractUser):
     pass
 
-
 class Category(models.Model):
     category = models.CharField(max_length=100)
 
     def __str__(self):
         return self.category
-
 
 class Listings(models.Model):
     title = models.CharField(max_length=100)
